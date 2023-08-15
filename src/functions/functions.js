eef15cde09f8c8e79d0bd1d16634bac8aaa2866b
kiilove <jbkim@jncore.com>
2023-08-15 20:14:18 +0900

    commit on "main"

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -10,3 +10,24 @@ export const generateToday = () => {
   const currentDateTime = dayjs().format("YYYY-MM-DD HH:mm");
   return currentDateTime;
 };
+
+export const handleMachineCheck = () => {
+  const savedCurrentContest = localStorage.getItem("currentContest");
+  const loading = false;
+  let error = { code: undefined, popupMessage: undefined };
+  const popupMessage = {
+    body: "기기 초기값이 설정되지 않았습니다.",
+    body2: "관리자 로그인페이지로 이동합니다.",
+    isButton: true,
+    confirmButtonText: "확인",
+  };
+
+  if (!savedCurrentContest) {
+    error = { code: "200", popupMessage };
+  }
+  const machineId = JSON.parse(savedCurrentContest).machineId;
+  const contestInfo = JSON.parse(savedCurrentContest).contests;
+
+  const msgOpen = true;
+  return { machineId, contestInfo, loading, error };
+};
