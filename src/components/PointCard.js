b28dd8d5d4c86a821faf250b484f3f3897a87d57
kiilove <jbkim@jncore.com>
2023-08-17 19:27:59 +0900

    commit on "main"

---
+++ src/components/PointCard.js
@@ -0,0 +1,7 @@
+export const PointRange = 10;
+export const PointArray = [
+  { id: 0, title: "외관이미지" },
+  { id: 1, title: "신체비율" },
+  { id: 2, title: "포징및 표현력" },
+  { id: 3, title: "근육과 선명도" },
+];
